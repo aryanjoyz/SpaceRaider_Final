017489296fe55b72842a16d83203122d3cb78b78
aryanjoyz <guilaran1820867@mkt.ceu.edu.ph>
2019-03-24 21:47:49 +0800

    healthbar

--- SpaceRaider_Final/src/com/game/src/main/Physics.java
+++ SpaceRaider_Final/src/com/game/src/main/Physics.java
@@ -6,21 +6,18 @@ import com.game.src.main.classes.Entity1;
 import com.game.src.main.classes.Entity2;
 
 public class Physics {
-	public static boolean Collision(Entity1 e1, LinkedList<Entity2> e2){
-		for(int i = 0; i < e2.size(); i++){
-			if (e1.getBounds().intersects(e2.get(i).getBounds())){
+	public static boolean Collision(Entity1 e1, Entity2 e2){
+			if (e1.getBounds().intersects(e2.getBounds())){
 				return true;
-			}
 		}
 		
 		return false;
 	}
 	
-	public static boolean Collision(Entity2 e2, LinkedList<Entity1> e1){
-		for(int i = 0; i < e1.size(); i++){
-			if (e2.getBounds().intersects(e1.get(i).getBounds())){
+	public static boolean Collision(Entity2 e2, Entity1 e1){
+			if (e2.getBounds().intersects(e1.getBounds())){
 				return true;
-			}
+
 		}
 		
 		return false;
